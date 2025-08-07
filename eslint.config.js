1d2845836510b8407e1cf319c6c10a8cd23b14a8
heihuafu <13076682658@163.com>
2025-08-08 00:12:15 +0800

    feat: init -依赖配置

---
+++ eslint.config.js
@@ -0,0 +1,67 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import importSort from "eslint-plugin-simple-import-sort"
+import pluginVue from 'eslint-plugin-vue';
+import vueEslintParser from 'vue-eslint-parser';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        // ...globals.browser, // 通常在推荐配置中已包含，可以省略
+        computed: 'readonly',
+        defineEmits: 'readonly',
+        defineExpose: 'readonly',
+        defineProps: 'readonly',
+        onMounted: 'readonly',
+        onUnmounted: 'readonly',
+        reactive: 'readonly',
+        ref: 'readonly',
+        shallowReactive: 'readonly',
+        shallowRef: 'readonly',
+        toRef: 'readonly',
+        toRefs: 'readonly',
+        watch: 'readonly',
+        watchEffect: 'readonly',
+      },
+    },
+    name: 'xxx/vue/setup',
+    plugins: {
+      vue: pluginVue,
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx,vue}'],
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginVue.configs['flat/recommended'].rules,
+      'no-console': 'error',
+      'no-undef': 'warn',
+      'no-unused-vars': 'error',
+      'vue/valid-define-emits': 'error',
+    },
+    languageOptions: {
+      parser: vueEslintParser,
+      parserOptions: {
+        extraFileExtensions: ['.vue'],
+        // ecmaVersion: 'latest',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        parser: tsParser,
+        sourceType: 'module',
+        // project: [
+        //   path.join(import.meta.dirname, 'tsconfig.eslint.json'),
+        //   path.join(import.meta.dirname, '../../**/tsconfig.json')
+        // ],
+        // // project: path.join(import.meta.dirname, 'tsconfig.eslint.json'),
+        // tsconfigRootDir: import.meta.dirname
+      },
+    },
+    plugins: {
+      vue: pluginVue,
+      'simple-import-sort': importSort,
+    },
+  },
+];
